c362b5afb13e184b9c96f4a48bd94238c189eb16
Petar Jovanovic <petar.jovanovic@rt-rk.com>
2013-10-16 03:11:06 +0200

    [MIPS] Move definitions of Mips_copy_relocs after Target_mips declaration
    Move definitions of Mips_copy_relocs methods after the declaration of
    Target_mips class. Methods of Mips_copy_relocs use methods of Target_mips
    class, and clang complains if declaration is not known when methods are
    called.
    No functional change.
    
    This issue was found when gold was sandboxed with pnacl toolchain.
    Part of the reported error was:
    
    error: implicit instantiation of undefined template
    '<anonymous>::Target_mips<32, false>'
    target->got_section(symtab, layout)->record_global_got_symbol(sym,
    this->relobj_, this->reloc_type_, true, false);
    
    BUG= clang reports an error when compiling gold
    TEST= build ld.nexe
    R=mseaborn@chromium.org
    
    Review URL: https://codereview.chromium.org/27200004

--- gold/mips.cc
+++ gold/mips.cc
@@ -1967,254 +1967,6 @@ class Mips_copy_relocs
 };
 
 
-// Mips_copy_relocs::Copy_reloc_entry methods.
-
-// Emit the reloc if appropriate.
-
-template<int sh_type, int size, bool big_endian>
-void
-Mips_copy_relocs<sh_type, size, big_endian>::Copy_reloc_entry::emit(
-    Output_data_reloc<sh_type, true, size, big_endian>* reloc_section,
-    Mips_copy_relocs<sh_type, size, big_endian>* copy_relocs,
-    Symbol_table* symtab, Layout* layout, Target_mips<size, big_endian> *target)
-{
-  // If the symbol is no longer defined in a dynamic object, then we
-  // emitted a COPY relocation, and we do not want to emit this
-  // dynamic relocation.
-  if (!this->sym_->is_from_dynobj())
-    return;
-
-  bool can_make_dynamic = this->reloc_type_ == elfcpp::R_MIPS_32
-                       || this->reloc_type_ == elfcpp::R_MIPS_REL32
-                       || this->reloc_type_ == elfcpp::R_MIPS_64;
-
-  Mips_symbol<size>* sym = Mips_symbol<size>::as_mips_sym(this->sym_);
-  if (can_make_dynamic && !sym->has_static_relocs())
-    {
-      target->got_section(symtab, layout)->record_global_got_symbol(sym, this->relobj_, 0, true, false);
-      if (!symbol_references_local (sym, should_add_dynsym_entry(sym, symtab)))
-        target->rel_dyn_section(layout)->add_global(sym, elfcpp::R_MIPS_REL32,
-            this->output_section_, this->relobj_, this->shndx_, this->address_);
-      else
-        target->rel_dyn_section(layout)->add_symbolless_global_addend(
-            sym, elfcpp::R_MIPS_REL32, this->output_section_, this->relobj_,
-            this->shndx_, this->address_);
-    }
-  else
-    copy_relocs->make_copy_reloc(symtab, layout,
-        static_cast<Sized_symbol<size>*>(this->sym_), reloc_section);
-}
-
-// Mips_copy_relocs methods.
-
-// Handle a relocation against a symbol which may force us to generate
-// a COPY reloc.
-
-template<int sh_type, int size, bool big_endian>
-void
-Mips_copy_relocs<sh_type, size, big_endian>::copy_reloc(
-    Symbol_table* symtab,
-    Layout* layout,
-    Sized_symbol<size>* sym,
-    Sized_relobj_file<size, big_endian>* object,
-    unsigned int shndx,
-    Output_section* output_section,
-    const Reloc& rel,
-    Output_data_reloc<sh_type, true, size, big_endian>* reloc_section)
-{
-  if (this->need_copy_reloc(sym, object, shndx))
-    this->make_copy_reloc(symtab, layout, sym, reloc_section);
-  else
-    {
-      // We may not need a COPY relocation.  Save this relocation to
-      // possibly be emitted later.
-      this->save(sym, object, shndx, output_section, rel);
-    }
-}
-
-// Return whether we need a COPY reloc for a relocation against SYM.
-// The relocation is begin applied to section SHNDX in OBJECT.
-
-template<int sh_type, int size, bool big_endian>
-bool
-Mips_copy_relocs<sh_type, size, big_endian>::need_copy_reloc(
-    Sized_symbol<size>* sym,
-    Sized_relobj_file<size, big_endian>* object,
-    unsigned int shndx) const
-{
-  if (!parameters->options().copyreloc())
-    return false;
-
-  if (sym->symsize() == 0)
-    return false;
-
-  // If this is a readonly section, then we need a COPY reloc.
-  // Otherwise we can use a dynamic reloc.  Note that calling
-  // section_flags here can be slow, as the information is not cached;
-  // fortunately we shouldn't see too many potential COPY relocs.
-  if ((object->section_flags(shndx) & elfcpp::SHF_WRITE) == 0)
-    return true;
-
-  return false;
-}
-
-// Emit a COPY relocation for SYM.
-
-template<int sh_type, int size, bool big_endian>
-void
-Mips_copy_relocs<sh_type, size, big_endian>::emit_copy_reloc(
-    Symbol_table* symtab,
-    Sized_symbol<size>* sym,
-    Output_data* posd,
-    off_t offset,
-    Output_data_reloc<sh_type, true, size, big_endian>* reloc_section)
-{
-  // Define the symbol as being copied.
-  symtab->define_with_copy_reloc(sym, posd, offset);
-
-  // Add the COPY relocation to the dynamic reloc section.
-  reloc_section->add_global_generic(sym, this->copy_reloc_type_, posd,
-                                    offset, 0);
-}
-
-// Make a COPY relocation for SYM and emit it.
-
-template<int sh_type, int size, bool big_endian>
-void
-Mips_copy_relocs<sh_type, size, big_endian>::make_copy_reloc(
-    Symbol_table* symtab,
-    Layout* layout,
-    Sized_symbol<size>* sym,
-    Output_data_reloc<sh_type, true, size, big_endian>* reloc_section)
-{
-  // We should not be here if -z nocopyreloc is given.
-  gold_assert(parameters->options().copyreloc());
-
-  typename elfcpp::Elf_types<size>::Elf_WXword symsize = sym->symsize();
-
-  // There is no defined way to determine the required alignment of
-  // the symbol.  We know that the symbol is defined in a dynamic
-  // object.  We start with the alignment of the section in which it
-  // is defined; presumably we do not require an alignment larger than
-  // that.  Then we reduce that alignment if the symbol is not aligned
-  // within the section.
-  gold_assert(sym->is_from_dynobj());
-  bool is_ordinary;
-  unsigned int shndx = sym->shndx(&is_ordinary);
-  gold_assert(is_ordinary);
-  typename elfcpp::Elf_types<size>::Elf_WXword addralign;
-
-  {
-    // Lock the object so we can read from it.  This is only called
-    // single-threaded from scan_relocs, so it is OK to lock.
-    // Unfortunately we have no way to pass in a Task token.
-    const Task* dummy_task = reinterpret_cast<const Task*>(-1);
-    Object* obj = sym->object();
-    Task_lock_obj<Object> tl(dummy_task, obj);
-    addralign = obj->section_addralign(shndx);
-  }
-
-  typename Sized_symbol<size>::Value_type value = sym->value();
-  while ((value & (addralign - 1)) != 0)
-    addralign >>= 1;
-
-  // Mark the dynamic object as needed for the --as-needed option.
-  sym->object()->set_is_needed();
-
-  if (this->dynbss_ == NULL)
-    {
-      this->dynbss_ = new Output_data_space(addralign, "** dynbss");
-      layout->add_output_section_data(".bss",
-                                      elfcpp::SHT_NOBITS,
-                                      elfcpp::SHF_ALLOC | elfcpp::SHF_WRITE,
-                                      this->dynbss_, ORDER_BSS, false);
-    }
-
-  Output_data_space* dynbss = this->dynbss_;
-
-  if (addralign > dynbss->addralign())
-    dynbss->set_space_alignment(addralign);
-
-  section_size_type dynbss_size =
-    convert_to_section_size_type(dynbss->current_data_size());
-  dynbss_size = align_address(dynbss_size, addralign);
-  section_size_type offset = dynbss_size;
-  dynbss->set_current_data_size(dynbss_size + symsize);
-
-  this->emit_copy_reloc(symtab, sym, dynbss, offset, reloc_section);
-}
-
-// Save a relocation to possibly be emitted later.
-
-template<int sh_type, int size, bool big_endian>
-void
-Mips_copy_relocs<sh_type, size, big_endian>::save(
-    Symbol* sym,
-    Sized_relobj_file<size, big_endian>* object,
-    unsigned int shndx,
-    Output_section* output_section,
-    const Reloc& rel)
-{
-  unsigned int reloc_type = elfcpp::elf_r_type<size>(rel.get_r_info());
-  typename elfcpp::Elf_types<size>::Elf_Addr addend =
-    Reloc_types<sh_type, size, big_endian>::get_reloc_addend_noerror(&rel);
-  this->entries_.push_back(Copy_reloc_entry(sym, reloc_type, object, shndx,
-                                            output_section, rel.get_r_offset(),
-                                            addend));
-}
-
-// Emit any saved relocs.
-
-template<int sh_type, int size, bool big_endian>
-void
-Mips_copy_relocs<sh_type, size, big_endian>::emit(
-    Output_data_reloc<sh_type, true, size, big_endian>* reloc_section,
-    Symbol_table* symtab, Layout* layout, Target_mips<size, big_endian> *target)
-{
-  for (typename Copy_reloc_entries::iterator p = this->entries_.begin();
-       p != this->entries_.end();
-       ++p)
-    p->emit(reloc_section, this, symtab, layout, target);
-
-  // We no longer need the saved information.
-  this->entries_.clear();
-}
-
-// Instantiate the templates we need.
-
-#ifdef HAVE_TARGET_32_LITTLE
-template
-class Mips_copy_relocs<elfcpp::SHT_REL, 32, false>;
-
-template
-class Mips_copy_relocs<elfcpp::SHT_RELA, 32, false>;
-#endif
-
-#ifdef HAVE_TARGET_32_BIG
-template
-class Mips_copy_relocs<elfcpp::SHT_REL, 32, true>;
-
-template
-class Mips_copy_relocs<elfcpp::SHT_RELA, 32, true>;
-#endif
-
-#ifdef HAVE_TARGET_64_LITTLE
-template
-class Mips_copy_relocs<elfcpp::SHT_REL, 64, false>;
-
-template
-class Mips_copy_relocs<elfcpp::SHT_RELA, 64, false>;
-#endif
-
-#ifdef HAVE_TARGET_64_BIG
-template
-class Mips_copy_relocs<elfcpp::SHT_REL, 64, true>;
-
-template
-class Mips_copy_relocs<elfcpp::SHT_RELA, 64, true>;
-#endif
-
-
 // Return true if the symbol SYM should be considered to resolve local
 // to the current module, and false otherwise. The logic is taken from
 // _bfd_elf_symbol_refs_local_p.
@@ -3441,6 +3193,254 @@ private:
   Mips16_stubs_map mips16_call_stubs_;
   Mips16_stubs_map mips16_call_fp_stubs_;
 };
+
+
+// Mips_copy_relocs::Copy_reloc_entry methods.
+
+// Emit the reloc if appropriate.
+
+template<int sh_type, int size, bool big_endian>
+void
+Mips_copy_relocs<sh_type, size, big_endian>::Copy_reloc_entry::emit(
+    Output_data_reloc<sh_type, true, size, big_endian>* reloc_section,
+    Mips_copy_relocs<sh_type, size, big_endian>* copy_relocs,
+    Symbol_table* symtab, Layout* layout, Target_mips<size, big_endian> *target)
+{
+  // If the symbol is no longer defined in a dynamic object, then we
+  // emitted a COPY relocation, and we do not want to emit this
+  // dynamic relocation.
+  if (!this->sym_->is_from_dynobj())
+    return;
+
+  bool can_make_dynamic = this->reloc_type_ == elfcpp::R_MIPS_32
+                       || this->reloc_type_ == elfcpp::R_MIPS_REL32
+                       || this->reloc_type_ == elfcpp::R_MIPS_64;
+
+  Mips_symbol<size>* sym = Mips_symbol<size>::as_mips_sym(this->sym_);
+  if (can_make_dynamic && !sym->has_static_relocs())
+    {
+      target->got_section(symtab, layout)->record_global_got_symbol(sym, this->relobj_, 0, true, false);
+      if (!symbol_references_local (sym, should_add_dynsym_entry(sym, symtab)))
+        target->rel_dyn_section(layout)->add_global(sym, elfcpp::R_MIPS_REL32,
+            this->output_section_, this->relobj_, this->shndx_, this->address_);
+      else
+        target->rel_dyn_section(layout)->add_symbolless_global_addend(
+            sym, elfcpp::R_MIPS_REL32, this->output_section_, this->relobj_,
+            this->shndx_, this->address_);
+    }
+  else
+    copy_relocs->make_copy_reloc(symtab, layout,
+        static_cast<Sized_symbol<size>*>(this->sym_), reloc_section);
+}
+
+// Mips_copy_relocs methods.
+
+// Handle a relocation against a symbol which may force us to generate
+// a COPY reloc.
+
+template<int sh_type, int size, bool big_endian>
+void
+Mips_copy_relocs<sh_type, size, big_endian>::copy_reloc(
+    Symbol_table* symtab,
+    Layout* layout,
+    Sized_symbol<size>* sym,
+    Sized_relobj_file<size, big_endian>* object,
+    unsigned int shndx,
+    Output_section* output_section,
+    const Reloc& rel,
+    Output_data_reloc<sh_type, true, size, big_endian>* reloc_section)
+{
+  if (this->need_copy_reloc(sym, object, shndx))
+    this->make_copy_reloc(symtab, layout, sym, reloc_section);
+  else
+    {
+      // We may not need a COPY relocation.  Save this relocation to
+      // possibly be emitted later.
+      this->save(sym, object, shndx, output_section, rel);
+    }
+}
+
+// Return whether we need a COPY reloc for a relocation against SYM.
+// The relocation is begin applied to section SHNDX in OBJECT.
+
+template<int sh_type, int size, bool big_endian>
+bool
+Mips_copy_relocs<sh_type, size, big_endian>::need_copy_reloc(
+    Sized_symbol<size>* sym,
+    Sized_relobj_file<size, big_endian>* object,
+    unsigned int shndx) const
+{
+  if (!parameters->options().copyreloc())
+    return false;
+
+  if (sym->symsize() == 0)
+    return false;
+
+  // If this is a readonly section, then we need a COPY reloc.
+  // Otherwise we can use a dynamic reloc.  Note that calling
+  // section_flags here can be slow, as the information is not cached;
+  // fortunately we shouldn't see too many potential COPY relocs.
+  if ((object->section_flags(shndx) & elfcpp::SHF_WRITE) == 0)
+    return true;
+
+  return false;
+}
+
+// Emit a COPY relocation for SYM.
+
+template<int sh_type, int size, bool big_endian>
+void
+Mips_copy_relocs<sh_type, size, big_endian>::emit_copy_reloc(
+    Symbol_table* symtab,
+    Sized_symbol<size>* sym,
+    Output_data* posd,
+    off_t offset,
+    Output_data_reloc<sh_type, true, size, big_endian>* reloc_section)
+{
+  // Define the symbol as being copied.
+  symtab->define_with_copy_reloc(sym, posd, offset);
+
+  // Add the COPY relocation to the dynamic reloc section.
+  reloc_section->add_global_generic(sym, this->copy_reloc_type_, posd,
+                                    offset, 0);
+}
+
+// Make a COPY relocation for SYM and emit it.
+
+template<int sh_type, int size, bool big_endian>
+void
+Mips_copy_relocs<sh_type, size, big_endian>::make_copy_reloc(
+    Symbol_table* symtab,
+    Layout* layout,
+    Sized_symbol<size>* sym,
+    Output_data_reloc<sh_type, true, size, big_endian>* reloc_section)
+{
+  // We should not be here if -z nocopyreloc is given.
+  gold_assert(parameters->options().copyreloc());
+
+  typename elfcpp::Elf_types<size>::Elf_WXword symsize = sym->symsize();
+
+  // There is no defined way to determine the required alignment of
+  // the symbol.  We know that the symbol is defined in a dynamic
+  // object.  We start with the alignment of the section in which it
+  // is defined; presumably we do not require an alignment larger than
+  // that.  Then we reduce that alignment if the symbol is not aligned
+  // within the section.
+  gold_assert(sym->is_from_dynobj());
+  bool is_ordinary;
+  unsigned int shndx = sym->shndx(&is_ordinary);
+  gold_assert(is_ordinary);
+  typename elfcpp::Elf_types<size>::Elf_WXword addralign;
+
+  {
+    // Lock the object so we can read from it.  This is only called
+    // single-threaded from scan_relocs, so it is OK to lock.
+    // Unfortunately we have no way to pass in a Task token.
+    const Task* dummy_task = reinterpret_cast<const Task*>(-1);
+    Object* obj = sym->object();
+    Task_lock_obj<Object> tl(dummy_task, obj);
+    addralign = obj->section_addralign(shndx);
+  }
+
+  typename Sized_symbol<size>::Value_type value = sym->value();
+  while ((value & (addralign - 1)) != 0)
+    addralign >>= 1;
+
+  // Mark the dynamic object as needed for the --as-needed option.
+  sym->object()->set_is_needed();
+
+  if (this->dynbss_ == NULL)
+    {
+      this->dynbss_ = new Output_data_space(addralign, "** dynbss");
+      layout->add_output_section_data(".bss",
+                                      elfcpp::SHT_NOBITS,
+                                      elfcpp::SHF_ALLOC | elfcpp::SHF_WRITE,
+                                      this->dynbss_, ORDER_BSS, false);
+    }
+
+  Output_data_space* dynbss = this->dynbss_;
+
+  if (addralign > dynbss->addralign())
+    dynbss->set_space_alignment(addralign);
+
+  section_size_type dynbss_size =
+    convert_to_section_size_type(dynbss->current_data_size());
+  dynbss_size = align_address(dynbss_size, addralign);
+  section_size_type offset = dynbss_size;
+  dynbss->set_current_data_size(dynbss_size + symsize);
+
+  this->emit_copy_reloc(symtab, sym, dynbss, offset, reloc_section);
+}
+
+// Save a relocation to possibly be emitted later.
+
+template<int sh_type, int size, bool big_endian>
+void
+Mips_copy_relocs<sh_type, size, big_endian>::save(
+    Symbol* sym,
+    Sized_relobj_file<size, big_endian>* object,
+    unsigned int shndx,
+    Output_section* output_section,
+    const Reloc& rel)
+{
+  unsigned int reloc_type = elfcpp::elf_r_type<size>(rel.get_r_info());
+  typename elfcpp::Elf_types<size>::Elf_Addr addend =
+    Reloc_types<sh_type, size, big_endian>::get_reloc_addend_noerror(&rel);
+  this->entries_.push_back(Copy_reloc_entry(sym, reloc_type, object, shndx,
+                                            output_section, rel.get_r_offset(),
+                                            addend));
+}
+
+// Emit any saved relocs.
+
+template<int sh_type, int size, bool big_endian>
+void
+Mips_copy_relocs<sh_type, size, big_endian>::emit(
+    Output_data_reloc<sh_type, true, size, big_endian>* reloc_section,
+    Symbol_table* symtab, Layout* layout, Target_mips<size, big_endian> *target)
+{
+  for (typename Copy_reloc_entries::iterator p = this->entries_.begin();
+       p != this->entries_.end();
+       ++p)
+    p->emit(reloc_section, this, symtab, layout, target);
+
+  // We no longer need the saved information.
+  this->entries_.clear();
+}
+
+// Instantiate the templates we need.
+
+#ifdef HAVE_TARGET_32_LITTLE
+template
+class Mips_copy_relocs<elfcpp::SHT_REL, 32, false>;
+
+template
+class Mips_copy_relocs<elfcpp::SHT_RELA, 32, false>;
+#endif
+
+#ifdef HAVE_TARGET_32_BIG
+template
+class Mips_copy_relocs<elfcpp::SHT_REL, 32, true>;
+
+template
+class Mips_copy_relocs<elfcpp::SHT_RELA, 32, true>;
+#endif
+
+#ifdef HAVE_TARGET_64_LITTLE
+template
+class Mips_copy_relocs<elfcpp::SHT_REL, 64, false>;
+
+template
+class Mips_copy_relocs<elfcpp::SHT_RELA, 64, false>;
+#endif
+
+#ifdef HAVE_TARGET_64_BIG
+template
+class Mips_copy_relocs<elfcpp::SHT_REL, 64, true>;
+
+template
+class Mips_copy_relocs<elfcpp::SHT_RELA, 64, true>;
+#endif
 
 
 //   R_MIPS16_26 is used for the mips16 jal and jalx instructions.
